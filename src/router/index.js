f2ff170c9ec8f2f7a71028e3065bfccef0650322
yangfan44 <yangfan44@baidu.com>
2017-12-06 17:10:32 +0800

    添加默认路由

--- src/router/index.js
+++ src/router/index.js
@@ -31,6 +31,10 @@ export default new Router({
               component: List
             }
           ]
+        },
+        {
+          path: '*',
+          redirect: '/home'
         }
       ]
     }
